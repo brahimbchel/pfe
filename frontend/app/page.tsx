2bca6999c3a602e6aaf470ffb9a6177083f3d6a3
brahimbchel <brahimbchel@gmail.com>
2025-03-09 00:21:02 +0100

    set up the nextjs app 2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-400 text-2xl">Hello L3 USTHB</h1>
+      <h1 className="text-red-400 text-2xl">Hello L3 USTHB ACAD C</h1>
     </>
   );
 }
